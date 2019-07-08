4da39472f5ee014e092d1b69a5033d28620bc4a5
Annisa Nabila Nasution <annisa.nasution@binus.ac.id>
2019-07-08 14:39:46 +0700

    Initial Commit

---
+++ AVFondation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFondation
+//
+//  Created by Annisa Nabila Nasution on 08/07/19.
+//  Copyright © 2019 Annisa Nabila Nasution. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
